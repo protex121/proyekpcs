7ed8715d9e178ea60fb272832d348e49d27c3fc3
gilbertkho <gilbertkho17@yahoo.com>
2019-05-23 22:16:57 +0700

    oracletransaction

--- master-proyek/master-proyek/Program.cs
+++ master-proyek/master-proyek/Program.cs
@@ -16,7 +16,7 @@ namespace master_proyek
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new formKasir());
         }
 
     }
